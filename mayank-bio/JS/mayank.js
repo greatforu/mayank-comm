10a50960110847eef27e2694ceb5c682cdb0c427
greatforu <36082945+greatforu@users.noreply.github.com>
2018-08-30 03:01:51 +0530

    Add files via upload

---
+++ mayank-bio/JS/mayank.js
@@ -0,0 +1,27 @@
+/**
+ * Created by aksha on 06-06-2
+ **/
+$(document).ready(function(){
+    $(".navbar a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    $(window).scroll(function() {
+        $(".slideanim").each(function(){
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+});
